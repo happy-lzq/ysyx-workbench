9f0481599d53ee05ec10080a1bc99ec448ab6f99
tracer-ysyx <tracer@ysyx.org>
2025-12-08 18:17:41 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:17:41 up 4 days, 18:53,  1 user,  load average: 0.57, 0.32, 0.35

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include <isa-def.h> 
 #include "local-include/reg.h"
 
 const char *regs[] = {
@@ -28,7 +29,8 @@ const char *regs[] = {
 
 void isa_reg_display() {
   int length = ARRLEN(regs);
-  
+    // 单独打印 PC
+  printf("pc  = 0x%08x\n", cpu.pc);
   // 打印通用寄存器 (GPR)，格式：4列 x 8行
   for (int i = 0; i < length; i++) {
     // 打印当前寄存器：名字占3字符，值占10字符(0x...)，制表符对齐
@@ -39,11 +41,7 @@ void isa_reg_display() {
       printf("\n");
     }
   }
-  
-  // 单独打印 PC
-  printf("pc  = 0x%08x\n", cpu.pc);
 }
-
 word_t isa_reg_str2val(const char *s, bool *success) {
   return 0;
 }
