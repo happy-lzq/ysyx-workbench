a0d49e1b79ff7503de62e6cb840b087c693ed4d2
tracer-ysyx <tracer@ysyx.org>
2025-12-14 14:49:26 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:26 up 10 days, 15:24,  1 user,  load average: 1.80, 2.04, 1.74

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -45,11 +45,12 @@ void isa_reg_display() {
 }
 word_t isa_reg_str2val(const char *s, bool *success) {
   int ok = 0;
-  if (strcmp(s+1,"pc")== 0){
+  const char *name = s +1;
+  if (strcmp(name,"pc")== 0){
     return cpu.pc;
   }
   for (int i = 0; i < 32; i++){
-    if (strcmp(s+1,regs[i]) == 0){
+    if (strcmp(name,regs[i]) == 0){
       return cpu.gpr[i];
     } 
   }
