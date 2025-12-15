ee98ef98a80861e71d16304c1fdef4bfe23fe223
tracer-ysyx <tracer@ysyx.org>
2025-12-16 00:25:33 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:25:33 up 12 days,  1:00,  1 user,  load average: 1.18, 1.80, 2.21

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -51,8 +51,8 @@ int main(int argc, char *argv[]) {
         printf("\ncurrent expr: %s\n",p);
         word_t val = expr(p, &success, &ishex);
         if (success) {
-          if (ishex) printf("0x%08" PRIx32 "\n", val);
-          else printf("%u\n", (unsigned)val);
+          if (ishex) printf("value: 0x%08" PRIx32 "\n", val);
+          else printf("value: %u\n", (unsigned)val);
         } else {
           printf("Bad expression: %s\n", p);
         }
