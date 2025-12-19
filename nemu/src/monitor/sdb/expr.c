854d87e41aeac89e8e6759a5fdfa1c1549cc3b9b
tracer-ysyx <tracer@ysyx.org>
2025-12-19 16:45:19 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:19 up 15 days, 17:20,  4 users,  load average: 0.59, 0.51, 0.56

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -68,7 +68,7 @@ static word_t parse_num(const char *s, bool *success){
 // ===================================指针对地址解引用=================================//
 static word_t get_pointer_value(const char *s, bool *success){
   word_t data;
-  word_t addr = parse_num(s,success);
+  word_t addr = parse_num(s+1,success);
   if (addr < 0x80000000 || addr >=0xffffffff){
     printf("输入解析地址不在地址范围内！请检查输入");
     *success = false;
