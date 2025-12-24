2be182e1fe7323b7d490c2c473ead14539b8e9b6
tracer-ysyx <tracer@ysyx.org>
2025-12-24 16:48:16 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:48:16 up 20 days, 17:23,  2 users,  load average: 0.55, 0.44, 0.47

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -17,26 +17,45 @@
 
 #define NR_WP 32
 
+// 链表节点定义
 typedef struct watchpoint {
   int NO;
+  char expr[256];
+  word_t prev_value;
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
 
 } WP;
 
+typedef struct wp_list
+{
+  WP* head;
+  WP* tail;
+  int size;
+}wp_list;
+static wp_list used_list = {NULL,NULL,0};
+static wp_list free_list = {NULL,NULL,0};
+// 利用结构体数组来表示监视点池，监视点池由两个链表组成：一个是空闲链表free，一个是使用中链表head
 static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
+    wp_pool[i].expr[0] = '\0';  
+    wp_pool[i].prev_value = 0;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
+  // free_list 未使用链表包含结构体数组监视池所有节点
+  free_list.head = &wp_pool[0];
+  free_list.tail = &wp_pool[NR_WP - 1];
+  free_list.size = NR_WP;
+  // used_list 初始化为空
+  used_list.head = NULL;
+  used_list.tail = NULL;
+  used_list.size = 0;
 
-  head = NULL;
-  free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
