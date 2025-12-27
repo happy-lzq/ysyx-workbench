b237d6854cd0d00661c417877e1faf0172fdc01e
tracer-ysyx <tracer@ysyx.org>
2025-12-27 14:41:36 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:36 up  1:28,  1 user,  load average: 1.27, 1.09, 0.83

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -79,11 +79,11 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-    // check_watchpoint 插入 ；检测到值变更新，并且打印所有值变表达式，同时改变nemu_state.state
-    if (check_watchpoint(&used_list) > 0){
+    if (nemu_state.state != NEMU_RUNNING) {
+      break;
+    }else if (check_watchpoint(&used_list) > 0){
       nemu_state.state = NEMU_STOP;
     }
-    if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
