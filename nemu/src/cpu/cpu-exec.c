2d6fe2e18ee9fa38e168e9bb93f2c407e4d0b5ea
tracer-ysyx <tracer@ysyx.org>
2025-12-29 11:02:14 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:14 up 1 day, 21:48,  3 users,  load average: 0.30, 0.14, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -77,13 +77,14 @@ static void execute(uint64_t n) {
   for (int i =0; i<n ; i++) {
     // 进入循环就开始检查，因为pc的变化是从进入exec_once进入取值之后就反馈pc= pc+4;
     // 即是进入第二次循环pc值已经改变了，所以循环初进行值变检测。在执行下一isa前设定nemu_state.state
-    if (check_watchpoint(&used_list) > 0){
-      nemu_state.state = NEMU_STOP;
-      break;
-    }
+
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
+    if (check_watchpoint(&used_list) > 0){
+      nemu_state.state = NEMU_STOP;
+      // break;
+    }
     if (nemu_state.state != NEMU_RUNNING) break;
 
     IFDEF(CONFIG_DEVICE, device_update());
