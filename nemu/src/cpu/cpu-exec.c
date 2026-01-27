2c708e227f6db81de70ec782130fcdc79e944394
tracer-ysyx <tracer@ysyx.org>
2026-01-27 16:18:38 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:38 up 1 day, 22:04,  2 users,  load average: 0.94, 0.56, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,11 +39,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-
-      if (check_watchpoint(&used_list) > 0){
-      nemu_state.state = NEMU_STOP;
-      //break;
-    }
+// 仅作分析，不要修改：
+if (nemu_state.state == NEMU_RUNNING && check_watchpoint(&used_list) > 0) {
+    nemu_state.state = NEMU_STOP;
+}
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
