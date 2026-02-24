03c018782c9b0935695baedcf3a7a8cfea5026da
tracer-ysyx <tracer@ysyx.org>
2026-02-24 21:22:30 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:30 up 1 day, 22:52,  1 user,  load average: 2.17, 1.22, 1.04

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -110,7 +110,11 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div    , R, R(rd) = ((src2 == 0 ) ? ~ 0 : 
                                                                         ((src1 == 0x80000000 && src2 == -1) ? 0x80000000 : 
                                                                         (int32_t) src1 / (int32_t) src2 ))) ;  
-  INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, R(rd) = (src2 == 0 ) ? ~0 : src1 / src2); 
+  INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, R(rd) = (src2 == 0 ) ? ~0: src1 / src2); 
+  INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, R(rd) = ((src2 == 0 ) ? src1 : 
+                                                                        ((src1 == 0x80000000 && src2 == -1) ? 0x00000000 : 
+                                                                        (int32_t) src1 % (int32_t) src2 ))) ;  
+  INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(rd) = (src2 == 0 ) ? src1: src1 % src2); 
 
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , R, R(rd) = src1 & src2);
   INSTPAT("0000000 ????? ????? 110 ????? 01100 11", or     , R, R(rd) = src1 | src2);
