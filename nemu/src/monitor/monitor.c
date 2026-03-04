6c8c7aaeda92bb5ecf1c8efb657532b065e52a1b
tracer-ysyx <tracer@ysyx.org>
2026-03-04 15:42:13 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:13 up  2:07,  1 user,  load average: 0.54, 0.78, 0.66

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -87,7 +87,7 @@ static int parse_args(int argc, char *argv[]) {
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-b,--batch              run with batch mode\n");      
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
