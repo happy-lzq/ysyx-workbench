45f7bbfcfbfb349ed07ee03385f9ef5486fedbe3
tracer-ysyx <tracer@ysyx.org>
2026-03-05 19:34:36 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:34:36 up 1 day,  5:59,  3 users,  load average: 0.79, 0.34, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,76 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *str = out;
+  /*
+  1、循环检查结束符
+  2、循环检查不等于字符串格式符%，str++
+  3、遇见%：跳过检查后接的符号进行分类
+  */
+  while (*fmt != '\0') {
+    if (*fmt != '%') {
+      *str++ = *fmt++;
+      continue;
+    }
+    fmt++; // skip '%'
+    if (*fmt == '\0') {
+      break;
+    }
+    switch (*fmt) {
+      case 's': {
+  // va_arg(va_list ap, type)：字符串格式决定type类型，进而决定va_arg()的返回类型，ap列表内存地址自动滑动相应数据类型内存宽度
+        const char *s = va_arg(ap, const char *);
+        if (!s) s = "(null)";
+        while (*s != '\0') {
+          *str++ = *s++;
+        }
+        break;
+      }
+      case 'd': {
+        int d = va_arg(ap, int);
+        if (d == 0) {
+          *str++ = '0';
+        } else {
+          unsigned int ud;  // 接受转化后的无符号正整数
+          if (d < 0) {
+            *str++ = '-';   // 负数在无符号整数前添加 -
+            ud = -d;
+          } else {
+            ud = d;
+          }
+// 把内存里的二进制正整数 ud，剥离出每一位的十进制位，转换为对应的 ASCII 字符，最后拼接到刚才的输出缓冲区里。
+          char buf[32];
+          int i = 0;
+          while (ud > 0) {
+            buf[i++] = (ud % 10) + '0'; // ASCII 字符'0' = 48；普通纯数字i(0-9)+字符'0' 刚好在ASCII码中表示对应的输出数字(0-9)
+            ud /= 10;
+          }
+          while (i > 0) {
+            *str++ = buf[--i];     // 将缓冲区的进制数调换成正确的位置()
+          }
+        }
+        break;
+      }
+      default: {
+        *str++ = '%';
+        *str++ = *fmt;
+        break;
+      }
+    }
+    fmt++;
+  }
+  *str = '\0';
+// printf、sprintf 等格式化输出家族函数的标准设定（也是 POSIX 标准规定）必须：
+// 返回成功写入缓冲区的字符总数（不包括结尾的 \0 结束符）。
+  return str - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
