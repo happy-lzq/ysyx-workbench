4ccfb568d8859893b53a0e84fa16ad655207ef0a
tracer-ysyx <tracer@ysyx.org>
2026-03-05 15:44:22 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:22 up 1 day,  2:09,  1 user,  load average: 1.47, 1.69, 1.34

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,23 +5,74 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len =0;
+  while (s[len] != '\0'){
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *pre_dst = dst;
+  while (*src != '\0')
+  {
+    *dst = *src;
+    dst++;
+    src++;
+  }
+  *dst = '\0';
+  return pre_dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *pre_dst = dst;
+  size_t i = 0;
+  while ((i < n) && src[i] !='\0' )
+  {
+    dst[i] = src[i];
+    i++;
+  }
+  for ( ; i < n; i++)
+  {
+    dst[i] = '\0';
+  }
+  return pre_dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *pre_dst = dst;
+  size_t i = 0;
+  while (dst[i] != '\0')
+  {
+    i++;
+  }
+  size_t y = 0;
+  while (src[y] !='\0')
+  {
+    dst[i] = src[y];
+    i++;
+    y++;
+  }
+  dst[i] = '\0';
+  return pre_dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while (*s1 == *s2)
+  {
+    if (*s1=='\0')
+    {
+      return 0;
+    }
+    s1++;
+    s2++;
+  }
+  /*
+  在 x86 架构的 GCC 编译器里，光写 char 通常等同于 signed char（有符号，范围是 -128 到 127）。
+  在 ARM 架构里，默认有时是 unsigned char（无符号，范围是 0 到 255）。
+
+  */
+  return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
