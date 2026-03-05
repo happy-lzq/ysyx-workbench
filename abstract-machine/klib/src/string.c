dddc94efa6aeb5bd4674a32957f5df2c741f57f6
tracer-ysyx <tracer@ysyx.org>
2026-03-05 17:15:31 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:31 up 1 day,  3:40,  1 user,  load average: 1.21, 1.51, 1.72

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -76,23 +76,78 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if (n == 0) return 0;
+  size_t i = 0;
+  while (*s1 == *s2 && i < n - 1)
+  {
+    if (*s1 == '\0')
+    {
+      return 0;
+    }
+    i++;
+    s1++;
+    s2++;
+  }
+  return (*(unsigned char*)s1 - *(unsigned char*)s2);   //n=1时直接计算第一个字符的ASIC码
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  //内存填充，指针指向前n个字节用int c 来填充
+  unsigned char* pre_s =  (unsigned char *) s;
+  size_t i = 0;
+  while (i < n)
+  {
+    pre_s[i] = (unsigned char) c ;
+    i++; 
+  }
+  return pre_s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  // 将源指针指向的内存位置的num字节 值复制到目标指针指向的内存块。复制过程如同使用中间缓冲区，允许源指针和目标指针重叠
+  unsigned char *pre_dst = (unsigned char *)dst;
+  const unsigned char *pre_src = (const unsigned char *)src;
+  size_t i=0;
+  // dst > src,从后往前移动
+  if (pre_dst > pre_src)
+  {
+     for ( i = n; i > 0; i--)
+     {
+  // 保证数组d[0]开始，并且循环成立，而且移动个数保证为n。
+      pre_dst[i-1] = pre_src[i-1];
+     }
+  } else {
+    for ( i = 0; i < n; i++)
+    {
+      pre_dst[i] = pre_src[i];
+    }
+    
+  }
+  return pre_dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  // 将源指针指向的内存位置的num字节 值直接复制到目标指针指向的内存块。dst 和 src 这两块内存绝对、绝对不可能有任何重合的部分！”
+  unsigned char *pre_out = (unsigned char *)out;
+  const unsigned char *pre_in = (const unsigned char *)in;
+  size_t i =0;
+  while (i < n)
+  {
+    pre_out[i] = pre_in[i];
+    i++;
+  }
+  return pre_out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char * pre_s1 = (const unsigned char *)s1;
+  const unsigned char * pre_s2 = (const unsigned char *)s2;
+  for (size_t i = 0; i < n; i++) {
+    if (pre_s1[i] != pre_s2[i]) {
+      return pre_s1[i] - pre_s2[i];
+    }
+  }
+  return 0;
 }
 
 #endif
