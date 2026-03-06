7c6e82c44aac0a5e9eb970698cc3ffa4732e03cb
tracer-ysyx <tracer@ysyx.org>
2026-03-06 20:05:24 +0800

    > compile NEMU SUAT25060203 罗智强 Linux zq 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:05:24 up 2 days,  6:30,  1 user,  load average: 1.39, 1.25, 1.04

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -39,31 +39,27 @@ static void welcome() {
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
 
-void sdb_set_batch_mode();
+void sdb_set_batch_mode();            // 外部批处理函数声明
 
-static char *log_file = NULL;
-static char *diff_so_file = NULL;
-static char *img_file = NULL;
-static int difftest_port = 1234;
+static char *log_file = NULL;         // 日志文件路径 对应（-l）
+static char *diff_so_file = NULL;     // 差分测试参考实现库路径，对应（-d）
+static char *img_file = NULL;         // 客户程序镜像路径
+static int difftest_port = 1234;      // 差分测试端口，对应（-p）
 
+// ================================== 加载客户程序镜像函数 ==========================================
 static long load_img() {
   if (img_file == NULL) {
     Log("No image is given. Use the default build-in image.");
     return 4096; // built-in image size
   }
-
   FILE *fp = fopen(img_file, "rb");
   Assert(fp, "Can not open '%s'", img_file);
-
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-
   Log("The image is %s, size = %ld", img_file, size);
-
   fseek(fp, 0, SEEK_SET);
   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
   assert(ret == 1);
-
   fclose(fp);
   return size;
 }
@@ -98,8 +94,7 @@ static int parse_args(int argc, char *argv[]) {
   return 0;
 }
 
-void 
-init_monitor(int argc, char *argv[]) {
+void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
 
   /* Parse arguments. */
