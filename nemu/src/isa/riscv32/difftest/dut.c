128f473cbbf0d2eacbfb52f8053694743e75f1fa
tracer-ysyx <tracer@ysyx.org>
2026-03-25 22:40:28 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:40:28 up  4:58,  1 user,  load average: 1.03, 1.01, 0.99

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,12 +18,6 @@
 #include <common.h>
 #include "../local-include/reg.h"
 
-const char *regs_name[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 
@@ -37,7 +31,8 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   {
     if (ref_r->gpr[i] != cpu.gpr[i])
     {
-      difftest_check_reg(regs_name[i],pc,ref_r->gpr[i],cpu.gpr[i]);
+      // 调用封装regs通用寄存器数组的指针函数 reg_name()
+      difftest_check_reg(reg_name(i),pc,ref_r->gpr[i],cpu.gpr[i]);
       return false;
     }
   }
