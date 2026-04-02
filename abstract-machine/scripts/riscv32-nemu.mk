96ae002b3f05a2cc8ea60a85499157faa6e34fe0
tracer-ysyx <tracer@ysyx.org>
2026-04-02 19:21:42 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:21:42 up 2 days, 25 min,  1 user,  load average: 1.19, 0.88, 1.21

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -3,7 +3,7 @@
 include $(AM_HOME)/scripts/isa/riscv.mk            #ISA指令架构脚本
 include $(AM_HOME)/scripts/platform/nemu.mk        #操作平台脚本
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-COMMON_CFLAGS += -march=rv32imc_zicsr -mabi=ilp32   # overwrite
+COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32   # overwrite
 LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \
