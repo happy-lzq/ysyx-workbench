17e4ed269e7d06cdc8e133881fab510126a412fc
tracer-ysyx <tracer@ysyx.org>
2026-04-06 17:15:02 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:15:02 up 2 days, 23:19,  1 user,  load average: 1.74, 1.05, 0.78

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -31,6 +31,10 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   printf("ysyx牛逼\n");
+    printf("ysyx牛逼\n");
+      printf("ysyx牛逼\n");
+        printf("ysyx牛逼\n");
+          printf("ysyx牛逼\n");
   am_init_monitor();
 #else
   init_monitor(argc, argv);
