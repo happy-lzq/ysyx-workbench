df2ee9141624ffd74cdf74d7ac3bcb59540729f7
tracer-ysyx <tracer@ysyx.org>
2026-04-06 19:24:38 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:24:38 up 3 days,  1:29,  1 user,  load average: 1.12, 1.14, 0.80

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
   printf("shabi luozhiqiang\n");
   am_init_monitor();
 #else
-printf("shabi luozhiqiang no amnemu\n");
+printf("\nshabi luozhiqiang no amnemu\n");
   init_monitor(argc, argv);
 #endif
 
