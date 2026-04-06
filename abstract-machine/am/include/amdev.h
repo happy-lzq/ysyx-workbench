090ca08ffb90a3985897d507c7b5126e43629d30
tracer-ysyx <tracer@ysyx.org>
2026-04-06 22:30:29 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:30:29 up 3 days,  4:34,  1 user,  load average: 1.70, 1.38, 1.20

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -7,14 +7,14 @@
   enum { AM_##reg = (id) }; \
   typedef struct { __VA_ARGS__; } AM_##reg##_T;
 
-AM_DEVREG( 1, UART_CONFIG,  RD, bool present);
-AM_DEVREG( 2, UART_TX,      WR, char data);
-AM_DEVREG( 3, UART_RX,      RD, char data);
-AM_DEVREG( 4, TIMER_CONFIG, RD, bool present, has_rtc);
+AM_DEVREG( 1, UART_CONFIG,  RD, bool present);                                // 指示串口是否存在（便于程序自适应）
+AM_DEVREG( 2, UART_TX,      WR, char data);                                   // 串口发送一个字节
+AM_DEVREG( 3, UART_RX,      RD, char data);                                   // 串口读取一个字节
+AM_DEVREG( 4, TIMER_CONFIG, RD, bool present, has_rtc);                       // 是否有实时时钟（RTC）
 AM_DEVREG( 5, TIMER_RTC,    RD, int year, month, day, hour, minute, second);  // 可读出当前的年月日时分秒. 
 AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);                                 // 系统启动后的微秒数
-AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);
-AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);                   // 键盘控制器-读出按键信息
+AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);                                // 否有键盘/按键设备
+AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);                   // keydown=true: 按下，反之释放
 AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);
 AM_DEVREG(10, GPU_STATUS,   RD, bool ready);
 AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
