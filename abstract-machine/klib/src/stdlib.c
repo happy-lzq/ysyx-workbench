5799d94e0e5f46d3d2993148a3f714a58f94c0f5
tracer-ysyx <tracer@ysyx.org>
2026-04-12 15:03:32 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:03:32 up 1 day, 23:50,  1 user,  load average: 1.07, 1.46, 1.90

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,63 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-typedef struct block_header {
-  size_t size;
-  int is_free;
-  struct block_header *next;
-} block_header_t;
-
-static block_header_t *heap_head = NULL;
-static char *heap_brk = NULL;
-
-static inline size_t align8(size_t x) {
-  return (x + 7) & ~((size_t)7);
-}
-
-static block_header_t *request_block(size_t size) {
-  if (heap_brk == NULL) {
-    heap_brk = (char *)heap.start;
-  }
-
-  size_t total = sizeof(block_header_t) + size;
-  char *new_brk = heap_brk + total;
-  if (new_brk > (char *)heap.end) {
-    return NULL;
-  }
-
-  block_header_t *blk = (block_header_t *)heap_brk;
-  blk->size = size;
-  blk->is_free = 0;
-  blk->next = NULL;
-  heap_brk = new_brk;
-  return blk;
-}
-
-static void split_block(block_header_t *blk, size_t size) {
-  if (blk->size < size + sizeof(block_header_t) + 8) {
-    return;
-  }
-
-  block_header_t *remain = (block_header_t *)((char *)(blk + 1) + size);
-  remain->size = blk->size - size - sizeof(block_header_t);
-  remain->is_free = 1;
-  remain->next = blk->next;
-
-  blk->size = size;
-  blk->next = remain;
-}
-
-static void coalesce_free_blocks(void) {
-  for (block_header_t *cur = heap_head; cur != NULL && cur->next != NULL; ) {
-    if (cur->is_free && cur->next->is_free) {
-      cur->size += sizeof(block_header_t) + cur->next->size;
-      cur->next = cur->next->next;
-    } else {
-      cur = cur->next;
-    }
-  }
-}
-
+static char *addr = NULL;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -87,56 +31,22 @@ int atoi(const char* nptr) {
 
 void *malloc(size_t size) {  
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  if (size == 0) {
-    return NULL;
+  // panic("Not implemented");
+  if (addr == NULL)
+  {
+    addr = (char*) heap.start;
   }
-
-  size = align8(size);
-
-  if (heap_head == NULL) {
-    block_header_t *blk = request_block(size);
-    if (blk == NULL) {
-      return NULL;
-    }
-    heap_head = blk;
-    return (void *)(blk + 1);
-  }
-
-  block_header_t *cur = heap_head;
-  block_header_t *last = NULL;
-  while (cur != NULL) {
-    if (cur->is_free && cur->size >= size) {
-      split_block(cur, size);
-      cur->is_free = 0;
-      return (void *)(cur + 1);
-    }
-    last = cur;
-    cur = cur->next;
-  }
-
-  block_header_t *blk = request_block(size);
-  if (blk == NULL) {
-    return NULL;
-  }
-  last->next = blk;
-  return (void *)(blk + 1);
+  size = (size + 7) & ~7;     // 按8字节对齐
+  void *ret = addr;
+  addr += size;
+  assert(addr <= (char*)heap.end );
+  return ret;
 #endif
   return NULL;
 }
 
 void free(void *ptr) {
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  if (ptr == NULL) {
-    return;
-  }
-
-  block_header_t *blk = ((block_header_t *)ptr) - 1;
-  assert((char *)blk >= (char *)heap.start && (char *)blk < (char *)heap.end);
-  blk->is_free = 1;
-  coalesce_free_blocks();
-#else
   (void)ptr;
-#endif
 }
 
 #endif
