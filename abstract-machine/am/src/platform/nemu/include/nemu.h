a1961f9cd1b809e4de4a92970e167cce1dd22c7a
tracer-ysyx <tracer@ysyx.org>
2026-04-16 01:14:54 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:14:54 up 1 day,  6:46,  1 user,  load average: 2.16, 1.63, 1.46

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -25,15 +25,15 @@
 #endif
 
 #define MMIO_BASE 0xa0000000    // 明确表明模拟器中设备空间映射到这个物理地址作为偏移起始地址
-// 内存映射I/O     外设真实必须存在外设寄存器/内存动态空间   与外设控制寄存器定义不同
+// 内存映射I/O
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)     // 串口数据端口内存映射
-#define KBD_ADDR        (DEVICE_BASE + 0x0000060)     // 键盘
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060)     //
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)     // 时钟计时器
-#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)     // VGA屏幕宽高配置
-#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)     // 声卡
-#define DISK_ADDR       (DEVICE_BASE + 0x0000300)     // 
-#define FB_ADDR         (MMIO_BASE   + 0x1000000)     // VGA帧同步缓冲区
-#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)     // 声卡音频缓冲区地址
+#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)     // VGA
+#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+#define DISK_ADDR       (DEVICE_BASE + 0x0000300)
+#define FB_ADDR         (MMIO_BASE   + 0x1000000)
+#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
 
 extern char _pmem_start;  //指示宿主进程中物理内存（pmem）起始地址对应的符号地址
 #define PMEM_SIZE (128 * 1024 * 1024)
