85ba1b3f74182523a2e872b6acf08d032f74e006
tracer-ysyx <tracer@ysyx.org>
2026-04-16 17:14:01 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:14:01 up 1 day, 22:45,  1 user,  load average: 2.54, 1.33, 1.12

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -19,15 +19,15 @@ AM_DEVREG( 5, TIMER_RTC,    RD, int year, month, day, hour, minute, second);  //
 AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);                                 // 系统启动后的微秒数
 AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);                                // 否有键盘/按键设备
 AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);                   // keydown=true: 按下，反之释放
-AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);  // 外设宽高等等
+AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);  
 AM_DEVREG(10, GPU_STATUS,   RD, bool ready);
-AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);         // 帧缓冲区
+AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);        
 AM_DEVREG(12, GPU_MEMCPY,   WR, uint32_t dest; void *src; int size);
 AM_DEVREG(13, GPU_RENDER,   WR, uint32_t root);
-AM_DEVREG(14, AUDIO_CONFIG, RD, bool present; int bufsize);    // 声卡是否存在，硬盘硬件缓冲区大小
-AM_DEVREG(15, AUDIO_CTRL,   WR, int freq, channels, samples);  // 
-AM_DEVREG(16, AUDIO_STATUS, RD, int count);                    // 
-AM_DEVREG(17, AUDIO_PLAY,   WR, Area buf);                     // 缓冲区
+AM_DEVREG(14, AUDIO_CONFIG, RD, bool present; int bufsize);   
+AM_DEVREG(15, AUDIO_CTRL,   WR, int freq, channels, samples); 
+AM_DEVREG(16, AUDIO_STATUS, RD, int count);                   
+AM_DEVREG(17, AUDIO_PLAY,   WR, Area buf);                    
 AM_DEVREG(18, DISK_CONFIG,  RD, bool present; int blksz, blkcnt);
 AM_DEVREG(19, DISK_STATUS,  RD, bool ready);
 AM_DEVREG(20, DISK_BLKIO,   WR, bool write; void *buf; int blkno, blkcnt);
