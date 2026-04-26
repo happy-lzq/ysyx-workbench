e9fa157caaf268beac3c984a5ee35fbf11059bf4
tracer-ysyx <tracer@ysyx.org>
2026-04-27 00:40:41 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:40:41 up  3:19,  1 user,  load average: 2.07, 1.85, 1.73

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -35,7 +35,8 @@ word_t isa_query_intr() {
     // 2. 定时器中断：mip.MTIP 和 mie.MTIE 同时为 1
     if ((cpu.csr[CSR_IDX_mip] & (M_TIME_MASK)) &&                 // mip寄存器中 MTIE(bit=7) 中断挂起位
         (cpu.csr[CSR_IDX_mie] & (M_TIME_MASK))) {                 // mie寄存器中 MTIP(bit=7) 中断是能位
-        return 0x80000007;                                        // M-mode 时钟中断号 7  最高位 = 1 表示中断，低 30 位 = 7 表示定时器
+         cpu.csr[CSR_IDX_mip] &= ~M_TIME_MASK;
+        return 0x80000007;                                      // M-mode 时钟中断号 7  最高位 = 1 表示中断，低 30 位 = 7 表示定时器
     }
 
     // 3. 外部中断：mip.MEIP 和 mie.MEIE 同时为 1                    
