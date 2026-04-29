3f09e8f643e79bc799acec3bc592a2ab8875f815
tracer-ysyx <tracer@ysyx.org>
2026-04-29 18:50:34 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:50:34 up  2:55,  1 user,  load average: 2.41, 1.80, 1.19

--- abstract-machine/am/src/platform/logisim/trm.c
+++ abstract-machine/am/src/platform/logisim/trm.c
@@ -5,6 +5,7 @@ extern char _heap_start;
 int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
+// 构建一个全局的堆区结构体，范围从 _heap_start 到 PMEM_END
 
 void putch(char ch) {
   outb(0x10000000, ch);
