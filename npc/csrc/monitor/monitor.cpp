a6878625095fcb414ded0a51e8c726286f012864
tracer-ysyx <tracer@ysyx.org>
2026-05-15 17:19:26 +0800

    > sim RTL ysyx_26010029 罗智强 Linux zq 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:19:26 up 1 day, 20:24,  1 user,  load average: 1.35, 1.31, 1.35

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -31,7 +31,7 @@ int parse_agrs(int argc,char *argv[]){
         {0      , 0                , NULL, 0 }
     };
     int o;
-    while ( (o = getopt_long(argc, argv, "-h:d:i:", table, NULL)) != -1) {
+    while ( (o = getopt_long(argc, argv, "-h:s:d:i:", table, NULL)) != -1) {
         switch (o) {
             case 'i' : img_file     = optarg; break;
             case 'd' : diff_so_file = optarg; break;
