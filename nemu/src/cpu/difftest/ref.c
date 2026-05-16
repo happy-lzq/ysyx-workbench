4d754853a48ece88bb801ebcd512fef3b6d68ecb
tracer-ysyx <tracer@ysyx.org>
2026-05-16 10:59:54 +0800

    > sim RTL ysyx_26010029 罗智强 Linux zq 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:59:54 up 2 days, 14:04,  1 user,  load average: 0.79, 0.97, 0.61

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -27,7 +27,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  printf("[REF regcpy] direction=%d, cpu.pc=0x%08x\n", direction, cpu.pc);
+  // printf("[REF regcpy] direction=%d, cpu.pc=0x%08x\n", direction, cpu.pc);
   if (direction == DIFFTEST_TO_REF){
     memcpy(&cpu,dut,DIFFTEST_REG_SIZE);
   } else{
