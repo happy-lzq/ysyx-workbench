07261967d7d4655cb4f9cdea6cd30652f28968a1
tracer-ysyx <tracer@ysyx.org>
2026-05-29 22:09:20 +0800

    > compile NEMU ysyx_26010029 罗智强 Linux zq 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:09:20 up 1 day,  8:17,  1 user,  load average: 2.68, 1.29, 1.09

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -55,9 +55,17 @@ static void fail(void *buf) { panic("access nonexist register"); }
 bool ioe_init() {
   for (int i = 0; i < LENGTH(lut); i++)
     if (!lut[i]) lut[i] = fail;
-  __am_gpu_init();            
+#ifdef CONFIG_HAS_TIMER
   __am_timer_init();
-  __am_audio_init();  
+#endif
+
+#ifdef CONFIG_HAS_VGA
+  __am_gpu_init();
+#endif
+
+#ifdef CONFIG_HAS_AUDIO
+  __am_audio_init();
+#endif
   return true;
 }
 
