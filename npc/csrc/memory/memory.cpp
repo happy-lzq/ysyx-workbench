8508fabc4f0b4b77731d1548fe0208963db6a6ff
tracer-ysyx <tracer@ysyx.org>
2026-06-03 12:57:28 +0800

    > sim RTL ysyx_26010029 罗智强 Linux zq 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:57:28 up 2 days, 21:02,  1 user,  load average: 1.59, 1.15, 0.88

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -66,7 +66,7 @@ int dpi_mem_read(int addr) {
     // MMIO 设备读
     mmio_accessed = true;
     switch (paddr) {
-        case 0x10000000: return 0;           // UART 只写设备
+        case 0x00003f8: return 0;           // UART 只写设备
         // case 0xa0000048: return rtc_lo(); // RTC（后续扩展）
         default: return 0;
         }
@@ -91,7 +91,7 @@ void dpi_mem_write(int addr, int data, int wmask) {
     // MMIO 设备写
     mmio_accessed = true;
     switch (paddr) {
-        case 0x10000000:  // UART (NEMU 兼容地址)
+        case 0x00003f8:  // UART (NEMU 兼容地址)
             if (wmask & 0x1)
                 npc_serial_putc(wdata & 0xFF);
             break;
