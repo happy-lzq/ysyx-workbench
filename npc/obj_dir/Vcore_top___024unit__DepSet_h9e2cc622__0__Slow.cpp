a1570d5e9925ca3cdfaf3c02693d6eed30c916f5
tracer-ysyx <tracer@ysyx.org>
2026-06-10 23:26:05 +0800

    > sim RTL ysyx_26010029 罗智强 Linux zq 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:26:05 up 12:56,  1 user,  load average: 0.34, 0.52, 0.30

---
+++ npc/obj_dir/Vcore_top___024unit__DepSet_h9e2cc622__0__Slow.cpp
@@ -0,0 +1,12 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vcore_top.h for the primary calling header
+
+#include "Vcore_top__pch.h"
+#include "Vcore_top___024unit.h"
+
+VL_ATTR_COLD void Vcore_top___024unit___ctor_var_reset(Vcore_top___024unit* vlSelf) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        Vcore_top___024unit___ctor_var_reset\n"); );
+    Vcore_top__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+}
